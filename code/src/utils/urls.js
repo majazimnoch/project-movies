abd0f2827dbe367d73189e1fdd6f4905dae3c346
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-03-28 15:45:24 +0200

    Add utils

---
+++ code/src/utils/urls.js
@@ -0,0 +1,4 @@
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/popular?api_key=39168ef639d8a2d49b6d7a9893ad1b8c&language=en-US&page=1';
+export const POPULAR_MOVIES_URL = 'https://api.themoviedb.org/3/movie/popular?api_key=39168ef639d8a2d49b6d7a9893ad1b8c&language=en-US&page=1';
+export const MOVIE_ID_URL = 'https://api.themoviedb.org/3/movie/{movie_id}?api_key=39168ef639d8a2d49b6d7a9893ad1b8c&language=en-US';
+export const RENDER_IMAGES_URL = 'https://api.themoviedb.org/3/configuration?api_key=39168ef639d8a2d49b6d7a9893ad1b8c';
